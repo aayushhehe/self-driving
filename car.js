dce83ba9f7ce49fbd0e13036d55d4563caabb8b6
Aayush <aayushbatham210036@acropolis.in>
2024-02-15 11:53:23 +0530

    update

--- car.js
+++ car.js
@@ -11,6 +11,7 @@ class Car {
     this.maxSpeed = 3;
     this.friction = 0.05;
     this.angle = 0;
+    this.damaged = false;
 
     this.sensor = new Sensor(this);
     this.controls = new Controls();
@@ -18,8 +19,46 @@ class Car {
 
   //update the car into the context of the canvas
   update(roadborders) {
-    this.#move();
+    if (!this.damaged) {
+      this.#move();
+      this.polygon = this.#createPolygon();
+      this.damaged = this.#assessDamage(roadborders);
+    }
+
     this.sensor.update(roadborders);
+  }
+
+  #assessDamage(roadborders) {
+    for (let i = 0; i < roadborders.length; i++) {
+      if (polysIntersect(this.polygon, roadborders[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  #createPolygon() {
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+
+    return points;
   }
 
   #move() {
@@ -71,15 +110,26 @@ class Car {
 
   //drawing the context onto the canvas
   draw(ctx) {
-    ctx.save(); //saves the canvas
-    ctx.translate(this.x, this.y);
-    ctx.rotate(-this.angle);
+    // ctx.save(); //saves the canvas
+    // ctx.translate(this.x, this.y);
+    // ctx.rotate(-this.angle);
 
+    // ctx.beginPath();
+    // ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    // ctx.fill();
+
+    // ctx.restore();
+    if (this.damaged) {
+      ctx.fillStyle = "gray";
+    } else {
+      ctx.fillStyle = "black";
+    }
     ctx.beginPath();
-    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    }
     ctx.fill();
-
-    ctx.restore();
 
     this.sensor.draw(ctx);
     //update the canvas
